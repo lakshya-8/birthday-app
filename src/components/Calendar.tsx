e3962efc95d01007446c7a4c37596bb79aef56c1
jtailor <jtailor@klipfolio.com>
2023-03-30 17:17:26 -0400

    fix: alignment fix

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -17,7 +17,8 @@ const CalendarWrapper = styled.div`
     margin-top: 50px;
     align-items: flex-start;
     margin-left: 20%;
-    margin-right: 20%;
+    margin-right: 20%;,
+    align-items: center;
 `;
 
 const Title = styled.h1`
